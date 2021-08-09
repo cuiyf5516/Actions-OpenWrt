1c5519dbbf1d43648cee9624702524ea275d4412
cuiyf5516 <yjcuiyf@gmail.com>
2021-08-09 09:35:16 +0800

    clone serverchan after install feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-cd package/lean
-git clone https://github.com/tty228/luci-app-serverchan.git
-cd -
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Clone serverchan
+cd package/lean
+git clone https://github.com/tty228/luci-app-serverchan.git
+cd -

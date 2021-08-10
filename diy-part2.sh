1a92080a10c378f8785f8adfc3f650b5680cf128
cuiyf5516 <48861298+cuiyf5516@users.noreply.github.com>
2021-08-10 13:56:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,4 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Clone serverchan
-cd package/lean
-git clone https://github.com/tty228/luci-app-serverchan.git
-cd -
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan

a611cc42a69d6af03d7d672460bd37d4be94d789
cuiyf5516 <yjcuiyf@gmail.com>
2026-03-04 17:48:48 +0800

    remove useless cmd

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generate
 
 # Clone serverchan
 # git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
